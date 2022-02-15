bc6da37c121c106387c5fa4098e03507199df3a6
Mike Munene <mike.munene@moringaschool.com>
2022-02-15 13:07:25 +0300

    Create movement for the battleship

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('battleship.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 
 def player(x,y):
@@ -28,17 +29,29 @@ while running:
 
     #RGB of background color
     screen.fill((0, 0, 0))
-    playerX += 0.2
+    
     
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+
+        # if keystroke is pressed check whether its right, left
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0 
     
     
 
-
+    playerX += playerX_change
     player(playerX, playerY)
     pygame.display.update()
 
