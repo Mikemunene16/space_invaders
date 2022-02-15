cfed18d9f2fe0433e22035d62aa931da4f8f5303
Mike Munene <mike.munene@moringaschool.com>
2022-02-15 12:44:47 +0300

    Add coordinates to the battleship

--- main.py
+++ main.py
@@ -11,11 +11,36 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon) 
 
+
+
+#Player
+playerImg = pygame.image.load('battleship.png')
+playerX = 370
+playerY = 480
+
+
+def player(x,y):
+    screen.blit(playerImg, (x, y))
+
 #GAME LOOP
 running = True
 while running:
+
+    #RGB of background color
+    screen.fill((0, 0, 0))
+    playerX += 0.2
+    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    
+    
+
+
+    player(playerX, playerY)
+    pygame.display.update()
+
 
 
