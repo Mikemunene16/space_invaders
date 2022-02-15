c930a749f85491cc4040818bbf6ecc7aeb078706
Mike Munene <mike.munene@moringaschool.com>
2022-02-15 12:06:18 +0300

    Create static folder

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ pygame.init()
 #Create the display and screen size
 screen = pygame.display.set_mode((800,600))
 
+#Title and Icon
+pygame.display.set_caption("Space Invaders") 
+# icon = pygame.image.load('../static/images/ufo.png')
+# pygame.display.set_icon(icon) 
+
 #GAME LOOP
 running = True
 while running:
